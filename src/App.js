bcdf3867a4dd4aa36a3ccdd6b61868954f9648fd
maJestic17 <77455932+maJestic17@users.noreply.github.com>
2021-09-27 00:04:29 +0300

    Added a GitHub URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by Daria Sukhacheva and is {""}
+      <a href="https://github.com/maJestic17/new-weather-app-react"
+      target="_blank">
+        open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   )
 }
