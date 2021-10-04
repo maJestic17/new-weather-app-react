d39027561da6b02e00bf6e5d9865a8ae586866d5
maJestic17 <77455932+maJestic17@users.noreply.github.com>
2021-10-04 18:54:52 +0300

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      Weather App
+      <Weather />
       <footer>
         This project was coded by Daria Sukhacheva and is {""}
       <a href="https://github.com/maJestic17/new-weather-app-react"
-      target="_blank">
+      target="_blank" rel="noreferrer">
         open-sourced on GitHub
       </a>
       </footer>
