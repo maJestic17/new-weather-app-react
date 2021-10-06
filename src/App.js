529aa40043f153623663ea97bea78ac3a9c7f535
maJestic17 <77455932+maJestic17@users.noreply.github.com>
2021-10-06 03:00:50 +0300

    installed axios, started using state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
+      <Weather defaultCity="New York" />
       <footer>
         This project was coded by Daria Sukhacheva and is {""}
       <a href="https://github.com/maJestic17/new-weather-app-react"
