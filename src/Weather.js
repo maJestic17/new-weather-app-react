0e37c4f0b4812bc7e3f9ed1bc8de94fda97e1291
maJestic17 <77455932+maJestic17@users.noreply.github.com>
2021-10-12 01:36:37 +0300

    Added Units Conversion

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ function handleResponse(response){
     date:new Date(response.data.dt*1000),
     wind:response.data.wind.speed,
     description:response.data.weather[0].description,
-    iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    icon: response.data.weather[0].icon,
     city: response.data.name
     
 }); 
