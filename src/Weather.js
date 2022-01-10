f6ee21ec5bc63132d571e4cf3c3452fc22886cfa
maJestic17 <77455932+maJestic17@users.noreply.github.com>
2022-01-10 04:19:45 +0300

    Made the Submit responsive to mobile devices

--- src/Weather.js
+++ src/Weather.js
@@ -43,15 +43,15 @@ if (weatherData.ready){
        <div className="Weather">
         <form onSubmit={handleSubmit}>
             <div className="row">
-                <div className="col-9">
+                <div className="col-xs-8 col-9">
             <input type="search" placeholder="Enter a city.."
             className="form-control"
             autoFocus="on"
             onChange={handleCityChange}
             />
             </div>
-             <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary w-100 col-auto" />
+             <div className="col-xs-4 col-3">
+            <input type="submit" value="Search" className="btn btn-primary btn-block" />
             </div>
             </div>
             </form>
